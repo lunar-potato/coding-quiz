cdeeb870842706024a2bbebfede66b6e9486b38e
luna potato <gwynethbautistaa@gmail.com>
2023-09-01 19:59:07 +0100

    Created pseudocode for JS for guide and referencing

---
+++ assets/js/logic.js
@@ -0,0 +1,28 @@
+// Import questions array from the questions.js
+
+// Define variables for current question index, user score,
+// timer value, timer interval ID
+
+// Function to start the quiz
+// Load in variables
+// Timer 
+
+// Function for rendering questions
+// Displaying the current question and choices
+// Event listeners for answer choice buttons
+
+// Function to user answer selection
+// Checking if selection is correct
+// Updating score and moving to next question
+// Subtracting time if the answer is incorrect
+
+// Function for timer (incorrect)
+// Decrementing timer value
+// Check if timer is zero, end quiz = true
+
+// Function for end quiz
+// Stop timer
+// Displaying user score
+// User score saving (initials and score)
+
+// Event listener to start button to start quiz
---
+++ assets/js/questions.js
@@ -0,0 +1,15 @@
+// Get elements for question area (questions + choices)
+
+// Object + Array for all questions and choices + right answer
+// Properties for question object include
+// Question
+// Array of answer choices
+// Index for the correct answer in the choices array
+
+// Function for shuffling questions
+// Copy of questions array
+// Shuffling syntax to reorder the copy (for loop)
+
+// Shuffle questions array 
+
+// Export/Run function of question objects
