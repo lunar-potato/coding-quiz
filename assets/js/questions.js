eed4f326801ac7c53efa21fadfce92580deca74f
luna potato <gwynethbautistaa@gmail.com>
2023-09-04 17:34:52 +0100

    Fixed questions.js to display questions properly and added code to shuffle choices

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,61 +1,97 @@
 window.questionsArea = [
-    {
-        questionText: "Which of the following is not an HTML tag?",
-        choices: ["Body", "Head", "Doctype", "br"],
-        answerIndex: 2,
-    },
-    {
-        questionText: "A collection of data containing both properties and methods is called...",
-        choices: ["Tag", "Selector", "Object", "Class"],
-        answerIndex: 2,
-    },
-    {
-        questionText: "What does HTML stand for?",
-        choices: ["HyperText Markup Language", "HyperTag Markup Language", "Hyperlinks Text Mark Language", "Hyperlinking Text Marking Language"],
-        answerIndex: 0,
-    },
-    {
-        questionText: "What is the correct tag for a line break?",
-        choices: ["<line>", "<bk>", "<brk>", "<br>"],
-        answerIndex: 3,
-    },
-    {
-        questionText: "What does CSS stand for?",
-        choices: ["Computing Style Sheet", "Creative Style System", "Cascading Style Sheet", "Creative Styling Sheet"],
-        answerIndex: 2,
-    },
-    {
-        questionText: "Where should a CSS file be referenced in a HTML file?",
-        choices: ["Before any HTML code", "After all HTML code", "Inside the head section", "Inside the body section"],
-        answerIndex: 2,
-    },
-    {
-        questionText: "What is the correct format for aligning written content to the center of the page in CSS?",
-        choices: ["text-align: center;", "font-align: center;", "text: align-center;", "font: align-center;"],
-        answerIndex: 0,
-    },
-    {
-        questionText: "What is the correct format for changing the background color of a div in CSS?",
-        choices: ["bg-color: red;", "background: red;", "background-colour: red;", "background-color: red;"],
-        answerIndex: 3,
-    },
-    {
-        questionText: "Which is the correct HTML tag for the largest heading?",
-        choices: ["<heading>", "<h1>", "<head>", "<title>"],
-        answerIndex: 1,
-    },
-    {
-        questionText: "Which symbols are used to indicate an end tag?",
-        choices: [">", "/", "*", "!"],
-        answerIndex: 1,
-    }
+  {
+    questionText: "Which of the following is not an HTML tag?",
+    choices: ["Body", "Head", "Doctype", "br"],
+    answer: "Doctype",
+  },
+  {
+    questionText:
+      "A collection of data containing both properties and methods is called...",
+    choices: ["Tag", "Selector", "Object", "Class"],
+    answer: "Object",
+  },
+  {
+    questionText: "What does HTML stand for?",
+    choices: [
+      "HyperText Markup Language",
+      "HyperTag Markup Language",
+      "Hyperlinks Text Mark Language",
+      "Hyperlinking Text Marking Language",
+    ],
+    answer:  "HyperText Markup Language",
+  },
+  {
+    questionText: "What is the correct tag for a line break?",
+    choices: ["<line>", "<bk>", "<brk>", "<br>"],
+    answer:  "<br>",
+  },
+  {
+    questionText: "What does CSS stand for?",
+    choices: [
+      "Computing Style Sheet",
+      "Creative Style System",
+      "Cascading Style Sheet",
+      "Creative Styling Sheet",
+    ],
+    answer: "Cascading Style Sheet",
+  },
+  {
+    questionText: "Where should a CSS file be referenced in a HTML file?",
+    choices: [
+      "Before any HTML code",
+      "After all HTML code",
+      "Inside the head section",
+      "Inside the body section",
+    ],
+    answer: "Inside the head section",
+  },
+  {
+    questionText:
+      "What is the correct format for aligning written content to the center of the page in CSS?",
+    choices: [
+      "text-align: center;",
+      "font-align: center;",
+      "text: align-center;",
+      "font: align-center;",
+    ],
+    answer:  "text-align: center;",
+  },
+  {
+    questionText:
+      "What is the correct format for changing the background color of a div in CSS?",
+    choices: [
+      "bg-color: red;",
+      "background: red;",
+      "background-colour: red;",
+      "background-color: red;",
+    ],
+    answer:  "background-color: red;",
+  },
+  {
+    questionText: "Which is the correct HTML tag for the largest heading?",
+    choices: ["<heading>", "<h1>", "<head>", "<title>"],
+    answer: "<h1>",
+  },
+  {
+    questionText: "Which symbols are used to indicate an end tag?",
+    choices: [">", "/", "*", "!"],
+    answer: "/",
+  },
 ];
 
 function shuffleQuestions(arr) {
-    for (let i = arr.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [arr[i], arr[j]] = [arr[j], arr[i]];
-    }
+  for (let i = arr.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
 }
 
+for (let i = 0; i < window.questionsArea.length; i++) {
+  window.questionsArea[i].choices = shuffleQuestions(
+    window.questionsArea[i].choices
+  );
+}
+
+console.log(window.questionsArea)
 shuffleQuestions(window.questionsArea);
--- assets/pseudocode.md
+++ assets/pseudocode.md
@@ -16,8 +16,9 @@ my script for the Coding Quiz.
 5. Shuffling syntax to reorder the copy (for loop)
 
 6. Shuffle questions array 
+7. Shuffle choices array
 
-7. Export/Run function of question objects
+8. Export/Run function of question objects
 
 ## Logic.js pseudocode
 1. Import questions array from the questions.js
@@ -47,6 +48,17 @@ my script for the Coding Quiz.
 19. User score saving (initials and score)
 
 20. Event listener to start button to start quiz
+
+## Scores.js pseudocode
+1. Variable for storing scores and initials
+
+2. Define function to save initials and score
+3. Prompt user to add their initials
+4. Store intials and score to an array
+
+5. Define function to display the leaderboard
+6. Retrieve the scores and initials
+7. Sort order by highest to lowest
 
 ## References 
 References and sources for questions are from:
